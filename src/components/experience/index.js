79b17a6e8d9392e4dea3b9e0c85a768eb5ce033a
Wagner Paulo <wagstalos@hotmail.com>
2021-10-09 15:01:11 -0300

    ajuste nas datas

--- src/components/experience/index.js
+++ src/components/experience/index.js
@@ -2,13 +2,13 @@ import { Chrono } from "react-chrono";
 
 export default function Experience() {
   const items = [{
-    title: "Agosto 2011",
+    title: "Ago 2011",
     cardTitle: "Estagiário Dev. Games",
     cardSubtitle:"(Graduando)",
     cardDetailedText: " Desenvolvimento de games como ActionScript, ajudava na produção de games em Flash.",
     
   },{
-    title: "Agosto 2012",
+    title: "Ago 2012",
     cardTitle: "Desenvolvedor de Games ",
     cardSubtitle:"(Graduando)",
     cardDetailedText: "Já era responsável pela parte de desenvolvimento de games da empresa, como primeira mudança resolvi mudar o desenvolvimento de games antes em Flash para HTML5! ",
@@ -20,14 +20,14 @@ export default function Experience() {
       cardDetailedText: "Liderava uma equipe para desenvolvimento de jogos educativos para clientes como: SER Educaional, Ânima, Sanofi, Globo entre outros.   ",
       
       },{
-        title: "Janeiro 2018",
+        title: "Mar 2018",
         cardTitle: "Lead Games e Desenvolvedor web ",
         cardSubtitle:"(DP Content)",
         cardDetailedText: "Comecei auxiliar também em alguns projetos web. Desenvolvimento de Landing Pages, Site corporativos, Emails Marketing, PWA e Wordpress",
       
       },
       {
-        title: "Janeiro 2021",
+        title: "Fev 2021",
         cardTitle: "Desenvolvedor Frontend ",
         cardSubtitle:"(Freelancer)",
         cardDetailedText: "Desenvolvimento ReactJS, Landing Pages, Site corporativos, Emails Marketing, PWA e Wordpress",
@@ -39,7 +39,7 @@ export default function Experience() {
   return(
     <>
       <div style={{ width: "100%", height: "auto" }}>
-        <Chrono items={items} mode="VERTICAL_ALTERNATING"  theme={{ 
+        <Chrono items={items} mode="VERTICAL"  theme={{ 
           primary: "#08b9bc",
           secondary: "#c9feff",
           cardBgColor: "#fff",
