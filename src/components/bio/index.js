ce77f7d59f866f58a489a61df5bd6c187cbe2136
Wagner Paulo <wagstalos@hotmail.com>
2022-11-30 10:18:42 -0300

    features: change avatar every 5 seconds

--- src/components/bio/index.js
+++ src/components/bio/index.js
@@ -1,6 +1,13 @@
 import "./style.css";
+import React, { useState, useEffect } from "react";
+
 import imageAvatar from "../../img/avatar.jpg";
 import imageRanger from "../../img/ranger.png";
+
+import a1 from "../../img/a1.jpg";
+import a2 from "../../img/a2.jpg";
+import a3 from "../../img/a3.jpg";
+import a4 from "../../img/a4.jpg";
 
 import Skills from "../skills";
 import Slider from "../slider";
@@ -9,14 +16,49 @@ import Toogle from "../toggle";
 import Experience from "../experience";
 import Whatsapp from "../whatsapp";
 
+const getPicture = (index) => {
+  switch (index) {
+    case 1:
+      return a1;
+
+    case 2:
+      return a2;
+
+    case 3:
+      return a3;
+
+    case 4:
+      return a4;
+
+    default:
+      return a1;
+  }
+};
+const NUMBER_OF_PICTURES = 2;
+
 function Bio() {
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setIndex((prevIndex) =>
+        index == NUMBER_OF_PICTURES ? 0 : prevIndex + 1
+      );
+    }, 5000);
+    return () => {
+      /* cleanup */
+      clearInterval(timer);
+    };
+    /* on component render*/
+  }, []);
+
   return (
     <div className="container" id="top">
       <div className="flex flex-column-mb">
         <div className="mr-50">
           <img
             className="img-bio blob blue"
-            src={imageAvatar}
+            src={getPicture(index)}
             alt="Imagem do autor"
           />
           <h5>Como me encontrar:</h5>
